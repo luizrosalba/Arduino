ef10e0457d0d821c67f2dd047dec0d4b8d8ccb77
Luiz Rosalba <luizrosalba@gmail.com>
2020-08-24 08:34:03 -0300

    Add files via upload

---
+++ temperatura.ino
@@ -0,0 +1,13 @@
+void setup() {
+
+pinMode(12, OUTPUT); //Declara que o pino 12 do arduino é de Saída. Vai mandar dados, energia...
+}
+
+void loop() {
+
+digitalWrite(12, HIGH);  // Diz que o pino 12 do arduino está Ligado. Logo LED ON
+delay(1000); // Espera por 1s
+println("nada") ; 
+digitalWrite(12, LOW); //  Diz que o pino 12 do arduino está Desligado. Logo: LED OFF
+delay(1000); // Espera por 1s
+}
