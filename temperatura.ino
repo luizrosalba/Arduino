19ad77bfd0212fcc6fffe86f7fcbcd0a840a67f7
Luiz Rosalba <luizrosalba@gmail.com>
2020-08-24 08:34:12 -0300

    Add files via upload

--- temperatura.ino
+++ temperatura.ino
@@ -1,13 +1,36 @@
-void setup() {
-
-pinMode(12, OUTPUT); //Declara que o pino 12 do arduino é de Saída. Vai mandar dados, energia...
+#include <DHT.h>
+ 
+#define DHTPIN A2  // o sensor dht11 foi conectado ao pino A1 no nosso tutorial
+#define DHTTYPE DHT11
+ 
+DHT dht(DHTPIN, DHTTYPE);
+ 
+void setup() 
+{
+Serial.begin(9600);
+Serial.println("DHTxx test!");
+dht.begin();
 }
-
-void loop() {
-
-digitalWrite(12, HIGH);  // Diz que o pino 12 do arduino está Ligado. Logo LED ON
-delay(1000); // Espera por 1s
-println("nada") ; 
-digitalWrite(12, LOW); //  Diz que o pino 12 do arduino está Desligado. Logo: LED OFF
-delay(1000); // Espera por 1s
+ 
+void loop() 
+{
+float umidade = dht.readHumidity();
+float temperatura = dht.readTemperature();
+// Se as variáveis temperatura e umidade não forem valores válidos, acusará falha de leitura.
+if (isnan(temperatura) || isnan(umidade)) 
+{
+Serial.println("Falha na leitura do dht11...");
+} 
+else 
+{
+ 
+//Imprime os dados no monitor serial
+Serial.print("Umidade: ");
+Serial.print(umidade);
+Serial.print(" %t"); //quebra de linha
+Serial.print("Temperatura: ");
+Serial.print(temperatura);
+Serial.println(" °C");
+ delay(3000);
+}
 }
