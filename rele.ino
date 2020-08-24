e5a4ec1d811f46e4e71ee7219f41ca3c6254b2af
Luiz Rosalba <luizrosalba@gmail.com>
2020-08-24 08:32:19 -0300

    Add files via upload

---
+++ rele.ino
@@ -0,0 +1,21 @@
+//Programa : Teste Modulo Rele Arduino 2 canais - Lampadas
+//Autor : FILIPEFLOP
+https://www.filipeflop.com/blog/controle-modulo-rele-arduino/
+ 
+//Porta ligada ao pino IN1 do modulo
+int porta_rele1 = 2;
+  
+void setup()
+{
+  //Define pinos para o rele como saida
+  pinMode(porta_rele1, OUTPUT); 
+
+}
+   
+void loop()
+{
+  digitalWrite(porta_rele1, LOW);  //Liga rele 1
+  delay(2000);
+  digitalWrite(porta_rele1, HIGH); //Desliga rele 1
+  delay(2000);
+}
